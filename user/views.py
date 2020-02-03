df92e6edbe220ad528774daede11aedd953673bd
qqs <qiushuangqian@sina.com>
2020-02-03 16:38:02 +0800

    Auth实现用户登录注册修改密码注销

--- user/views.py
+++ user/views.py
@@ -28,6 +28,24 @@ def loginView(request):
             tips = '用户不存在，请注册'
     return render(request, 'user.html', locals())
 
+#注册
+def registerView(request):
+    title = '注册'
+    unit_2 = '/user/login.html'
+    unit_2_name = '立即登录'
+    unit_1 = '/user/setpassword.html'
+    unit_1_name = '修改密码'
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        if User.objects.filter(username = username):
+            tips = "用户已存在"
+        else:
+            user = User.objects.create_user(username=username,password=password)
+            user.save()
+            tips = "注册成功，请登录"
+    return render(request,'user.html',locals())
+
 
 # 修改密码
 def setpasswordView(request):
